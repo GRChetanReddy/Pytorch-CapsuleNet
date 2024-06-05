894659d5080efc5aa3b7bd32fe68ffcdcd859487
GRChetanReddy <94971159+GRChetanReddy@users.noreply.github.com>
2024-06-05 11:19:33 +0530

    test_capsnet.py

--- test_capsnet.py
+++ test_capsnet.py
@@ -67,7 +67,28 @@ class Config:
             self.input_width = 32
             self.input_height = 32
 
-        elif dataset == 'your own dataset':
+        elif dataset == 'MURA':
+            # CNN (cnn)
+            self.cnn_in_channels = 3
+            self.cnn_out_channels = 256
+            self.cnn_kernel_size = 9
+
+            # Primary Capsule (pc)
+            self.pc_num_capsules = 8
+            self.pc_in_channels = 256
+            self.pc_out_channels = 32
+            self.pc_kernel_size = 9
+            self.pc_num_routes = 32 * 8 * 8
+
+            # Digit Capsule (dc)
+            self.dc_num_capsules = 10
+            self.dc_num_routes = 32 * 8 * 8
+            self.dc_in_channels = 8
+            self.dc_out_channels = 16
+
+            # Decoder
+            self.input_width = 32
+            self.input_height = 32
             pass
 
 
